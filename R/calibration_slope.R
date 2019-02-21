785777c0c4c0479bb5266f6eecba5ce9161726be
Sarah Haile <sarah.haile@uzh.ch>
2019-02-21 18:19:15 +0100

    add other performance measures (still need to document)

--- R/calibration_slope.R
+++ R/calibration_slope.R
@@ -12,3 +12,45 @@ calibration_slope <- function(bss, fm){
     m <- glm(fm, analysis(bss), family = binomial)
     coef(m)[2]
 }
+
+calibration.large <- function(bss, fm){
+    fm <- as.formula(fm)
+    fm <- update(fm, . ~ offset(qlogis(.)))
+    m <- glm(fm, analysis(bss), family = binomial)
+    coef(m)
+}
+
+c.statistic <- function(bss, fm){
+    require(pROC)
+    fm <- as.formula(fm)
+    m <- roc(fm, analysis(bss))
+    auc(m)
+}
+
+oe.ratio <- function(bss, fm){
+    if(is.character(fm)){
+        fm <- trimws(strsplit(fm, "~")[[1]])
+    } else {
+        fm <- as.character(fm)
+        fm <- fm[fm != "~"]
+    }
+    this.bss <- analysis(bss) %>%
+        select(fm) %>% 
+        drop_na()
+    obs <- this.bss %>% pull(fm[1])
+    pred <- this.bss %>% pull(fm[2])
+    sum(obs) / sum(pred)
+}
+
+brier.score <- function(bss, fm){
+    if(is.character(fm)){
+        fm <- trimws(strsplit(fm, "~")[[1]])
+    } else {
+        fm <- as.character(fm)
+        fm <- fm[fm != "~"]
+    }
+    obs <- analysis(bss)[, fm[1]]
+    pred <- analysis(bss)[, fm[2]]
+    brier.score <- (obs - pred) ^ 2
+    mean(brier.score, na.rm = TRUE)
+}
