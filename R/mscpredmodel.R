10d331fdbc18e2492e14b090dec4b02cf64dd9f9
Sarah Haile <sarah.haile@uzh.ch>
2019-02-25 12:46:40 +0100

    finish list of main functions

--- R/mscpredmodel.R
+++ R/mscpredmodel.R
@@ -12,6 +12,8 @@
 #'   \item{\code{\link{get_bs_samples}}}{Generate bootstrap samples, stratified by cohort}
 #'   \item{\code{\link{compute_performance}}}{Compute a performance measure for each of the scores, stratified by cohort, in each of the bootstrap samples. One such performance measure would be, for example, \code{\link{calibration_slope}}}
 #'   \item{\code{\link{aggregate_performance}}}{Produces the aggregated performance and its empirically estimated variance-covariance matrix for each cohort, to be used in the (in)consistency models.}
+#'   \item{\code{\link{consistency}}}{Computes the (in)consistency model.}
+#'   \item{\code{\link{msc_full}}}{Based on the raw performance estimates from \code{\link{compute_performance}}, compute direct (\code{\link{msc_direct}}), indirect (\code{\link{msc_indirect}}), or full network (\code{\link{msc_network}}) pairwise comparisons. Using \code{\link{msc_full}}, all three sets of pairwise estimates are computed at once.}
 #' }
 #'
 #' @docType package
