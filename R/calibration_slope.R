4687ada078931c2579fbd5cbb495d23cfeca4f88
Sarah Haile <sarah.haile@uzh.ch>
2019-02-25 10:46:13 +0100

    documentation for performance measure functions

--- R/calibration_slope.R
+++ R/calibration_slope.R
@@ -1,11 +1,17 @@
-#' Compute calibration slope
+#' Compute performance in terms of various typical calibration and discrimination measures.
+#' 
+#' Here we provide several functions to compute some typical performance measures 
+#' for calibration and discrimination. This is however not intended to be an 
+#' exhaustive set of performance measures.
 #'
 #' @param bss A set of bootstrap samples, stratified by cohort, as computed by \code{\link{get_bs_samples}}. The full bootstrap data is called within the function as \code{analysis(bss)}. See \code{\link{?rsample::bootstraps}} for more details.
 #' @param fm The formula that will be called by the model, of the form \code{outcome ~ score} (character).
 #'
-#' @return A single calibration slope (numeric)
+#' @return A single performance measure (numeric). 
 #' @export
 #'
+#' @describeIn calibration_slope Estimate calibration slope
+#' @export
 calibration_slope <- function(bss, fm){
     fm <- as.formula(fm)
     fm <- update(fm, . ~ qlogis(.))
@@ -13,21 +19,27 @@ calibration_slope <- function(bss, fm){
     coef(m)[2]
 }
 
-calibration.large <- function(bss, fm){
+#' @describeIn calibration_slope Estimate calibration-in-the-large
+#' @export
+calibration_large <- function(bss, fm){
     fm <- as.formula(fm)
     fm <- update(fm, . ~ offset(qlogis(.)))
     m <- glm(fm, analysis(bss), family = binomial)
     coef(m)
 }
 
-c.statistic <- function(bss, fm){
+#' @describeIn calibration_slope Estimate c-Statistics / Area under the ROC curve
+#' @export
+c_statistic <- function(bss, fm){
     require(pROC)
     fm <- as.formula(fm)
     m <- roc(fm, analysis(bss))
     auc(m)
 }
 
-oe.ratio <- function(bss, fm){
+#' @describeIn calibration_slope Estimate ratio of observed to expected number of events
+#' @export
+oe_ratio <- function(bss, fm){
     if(is.character(fm)){
         fm <- trimws(strsplit(fm, "~")[[1]])
     } else {
@@ -42,7 +54,9 @@ oe.ratio <- function(bss, fm){
     sum(obs) / sum(pred)
 }
 
-brier.score <- function(bss, fm){
+#' @describeIn calibration_slope Estimate Brier score
+#' @export
+brier_score <- function(bss, fm){
     if(is.character(fm)){
         fm <- trimws(strsplit(fm, "~")[[1]])
     } else {
