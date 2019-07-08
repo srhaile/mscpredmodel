58ee559c12ca69c0de6fdd7363aecf1bce22f379
Sarah Haile <sarah.haile@gmail.com>
2019-07-08 17:24:09 +0200

    added ICI and related measures for binary outcomes

--- R/calibration_slope.R
+++ R/calibration_slope.R
@@ -1,12 +1,17 @@
 #' Compute performance in terms of various typical calibration and discrimination measures.
 #' 
 #' Here we provide several functions to compute some typical performance measures 
-#' for calibration and discrimination. This is however not intended to be an 
+#' for calibration and discrimination (calibration slope, calibration-in-the-large, 
+#' c-Statistic / AUC, O:E ratio, Brier score, integrated calibration index, and 
+#' related E50, E90, and Emax). This is however not intended to be an 
 #' exhaustive set of performance measures.
 #' 
 #' Please note that \code{\link{compute_performance}} will use \code{try}, and return \code{NA} if the function has an error. Of the potentially large number of cohort, score and bootstrap sample, this was a straightforward way to prevent \code{\link{compute_performance}} from returning an error for the whole dataset. However, if the function to compute performance generally does not work (for example, if the package \code{pROC} is required but not loaded), this behavior also prevents warning message for, say, unloaded R packages from being printed. See the example for how to redefine the provided functions with a transformation. 
 #' 
-#' @importFrom stats model.matrix pnorm qnorm glm qlogis offset update na.omit as.formula coef
+#' @seealso Austin, PC, Steyerberg, EW. The Integrated Calibration Index (ICI) and related metrics for quantifying the calibration of logistic regression models. Statistics in Medicine. 2019; 1– 15. https://doi.org/10.1002/sim.8281
+#' 
+#' @seealso Debray, T. P., Damen, J. A., Riley, R. D., Snell, K., Reitsma, J. B., Hooft, L., … Moons, K. G. (2018). A framework for meta-analysis of prediction model studies with binary and time-to-event outcomes. Statistical Methods in Medical Research. https://doi.org/10.1177/0962280218785504
+#' @importFrom stats model.matrix pnorm qnorm glm qlogis offset update na.omit as.formula coef predict quantile loess
 #' @importFrom pROC roc auc
 #' 
 #' @param dd A dataset. 
@@ -87,3 +92,74 @@ brier_score <- function(dd, fm){
     brier.score <- (obs - pred) ^ 2
     mean(brier.score, na.rm = TRUE)
 }
+
+#' @describeIn calibration_slope Estimate Integrated Calibration Index (ICI) (mean)
+#' @aliases ICI
+#' @export
+int_calib_index <- function(dd, fm){
+    if(is.character(fm)){
+        fm <- trimws(strsplit(fm, "~")[[1]])
+    } else {
+        fm <- as.character(fm)
+        fm <- fm[fm != "~"]
+    }
+    obs <- dd[, fm[1]]
+    pred <- dd[, fm[2]]
+    loess.calibrate <- loess(obs ~ pred)
+    P.calibrate <- predict(loess.calibrate, newdata = pred)
+    p.diff <- abs(P.calibrate - pred)
+    mean(p.diff, na.rm = TRUE)
+}
+
+#' @describeIn calibration_slope Estimate Integrated Calibration Index (ICI) (median)
+#' @export
+E50 <- function(dd, fm){
+    if(is.character(fm)){
+        fm <- trimws(strsplit(fm, "~")[[1]])
+    } else {
+        fm <- as.character(fm)
+        fm <- fm[fm != "~"]
+    }
+    obs <- dd[, fm[1]]
+    pred <- dd[, fm[2]]
+    loess.calibrate <- loess(obs ~ pred)
+    P.calibrate <- predict(loess.calibrate, newdata = pred)
+    p.diff <- abs(P.calibrate - pred)
+    median(p.diff, na.rm = TRUE)
+}
+
+#' @describeIn calibration_slope Estimate Integrated Calibration Index (ICI) (90th percentile)
+#' @export
+E90 <- function(dd, fm){
+    if(is.character(fm)){
+        fm <- trimws(strsplit(fm, "~")[[1]])
+    } else {
+        fm <- as.character(fm)
+        fm <- fm[fm != "~"]
+    }
+    obs <- dd[, fm[1]]
+    pred <- dd[, fm[2]]
+    loess.calibrate <- loess(obs ~ pred)
+    P.calibrate <- predict(loess.calibrate, newdata = pred)
+    p.diff <- abs(P.calibrate - pred)
+    quantile(p.diff, probs = 0.9, na.rm = TRUE)
+}
+
+
+#' @describeIn calibration_slope Estimate Integrated Calibration Index (ICI) (maximum)
+#' @export
+Emax <- function(dd, fm){
+    if(is.character(fm)){
+        fm <- trimws(strsplit(fm, "~")[[1]])
+    } else {
+        fm <- as.character(fm)
+        fm <- fm[fm != "~"]
+    }
+    obs <- dd[, fm[1]]
+    pred <- dd[, fm[2]]
+    loess.calibrate <- loess(obs ~ pred)
+    P.calibrate <- predict(loess.calibrate, newdata = pred)
+    p.diff <- abs(P.calibrate - pred)
+    max(p.diff, na.rm = TRUE)
+}
+
