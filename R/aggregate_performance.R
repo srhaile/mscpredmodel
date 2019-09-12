dad2277429adcfac26813823b829ddc64af0f1ed
Sarah Haile <sarah.haile@uzh.ch>
2019-09-12 15:56:28 +0200

    Fix documentation for aggregation of moderators

--- R/aggregate_performance.R
+++ R/aggregate_performance.R
@@ -6,7 +6,7 @@
 #' @param perf.estimates A set of performance estimates of class \code{mscraw}, as computed by \code{\link{compute_performance}}
 #' @param reference The name of the reference score (default NULL, the first score is the reference). This will be the reference level of the scores in any later models.
 #' @param design.levels A character vector of alternate short names for the scores, to be used in naming the designs. Default is \code{\link{LETTERS}}, so that a possible design would be \code{AB} or \code{ACD}, instead of \code{score1score2} or \code{score1score3score4}. The design variable is used in the definition of the random effects. See \code{\link{consistency}}.
-#' @param fn.mods A function used to summarize the moderators within cohorts. If NULL, the default mean(x, na.rm = TRUE) is used.
+#' @param fn.mods A function used to summarize the moderators within cohorts. Moderators should be numeric as the mean is computed by default. Factor variables should converted to numeric prior to use in \code{\link{get_bs_samples}}. For example, if variable x2 has levels A, B, C and D, convert to x2f = 1 if x2 = {A, B} and 0, otherwise. If NULL, the default mean(x, na.rm = TRUE) is used.
 #' @param x An object of class \code{mscagg}, returned from \code{\link{aggregate_performance}}.
 #' @param ... Other arguments to be passed to \code{\link{print}}.
 #'
@@ -25,16 +25,19 @@
 #'   \item{mods}{A vector of names of the moderators}
 #'   \item{moderators}{A dataframe containing aggregated moderators}
 #' }
+#' 
+#' @details The aggregation of moderators here is different than what the function \code{\link{casemix}} is doing. \code{\link{casemix}} shows the actual levels of the moderators, either as median and IQR (or boxplots) for continuous variables, or tables (or barplots) for factor variables. In \code{aggregate_performance} we assume a single number is needed to describe the overall value of the moderator, which is most commonly the mean. Factor variables with two levels (e.g. A and B) should be converted to 0/1 variables. Unordered factor variables with more than 2 levels (e.g. A, B, C, and D) should be collapsed to 0/1 variables (for example 0 = {A, B}, 1 = {C, D}). Ordered factor variables (e.g. I, II, III, IV) should be converted to numeric variables (e.g. 1, 2, 3, 4). Any factor variables will be dropped in \code{aggregate_performance}.
+#' 
 #' @export
 #'
 #' @examples
 #' dat <- msc_sample_data()
-#' bssamp <- get_bs_samples(dat, id, study, outcome, n.samples = 10, 
+#' bssamp <- get_bs_samples(dat, id, study, outcome, n.samples = 5, 
 #'                   scores = c("a", "b", "c", "d", "e", "f"), 
-#'                   moderators = c("age", "female", "x1"))
+#'                   moderators = c("age", "female", "x1", "sex"))
 #' perf <- compute_performance(bssamp, fn = calibration_slope, lbl = "CS")
 #' agg <- aggregate_performance(perf)
-#' agg
+#' head(agg)
 aggregate_performance <- function(perf.estimates, reference = NULL,
                                   design.levels = LETTERS,
                                   fn.mods = NULL){
@@ -51,6 +54,16 @@ aggregate_performance <- function(perf.estimates, reference = NULL,
         warning("reference score was not one of the scores. The reference has been changed to ", scores[1])
         reference <- scores[1]
     }
+    
+    classmods <- sapply(moderators[[1]], class)
+    check.classmods <- classmods[mods] != "factor"
+    
+    if(any(!check.classmods)){
+      message("Removing moderators classified as factors: ", paste(names(check.classmods)[!check.classmods], collapse = ", "), 
+              "; consider recoding these to numeric")
+      mods <- names(check.classmods)[check.classmods]
+    }
+    
     
     if(is.null(fn.mods) & !is.null(mods)){
         #message("Since fn.mods was not specified, moderators will be aggregated with mean(x, na.rm = TRUE).")
