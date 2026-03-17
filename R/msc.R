417de5461569fcbf7f4cca739dd9b6099098bc34
Sarah Haile <sarah.haile@uzh.ch>
2026-03-17 16:28:23 +0100

    check contrmat output

--- R/msc.R
+++ R/msc.R
@@ -528,6 +528,7 @@ fit_msc <- function(scores = c("A", "B", "C", "D"),
         
         if(is.null(direct) & is.null(indirect)){
             aggr_ipd <- contrmat(aggr_ipd, "score.1", "score.2")
+            print(aggr_ipd)
             this_fm <- paste(scores[-1], collapse = " + ")
             this_fm <- paste("~", this_fm, "- 1")
         } else if(!is.null(direct) & is.null(indirect)){
